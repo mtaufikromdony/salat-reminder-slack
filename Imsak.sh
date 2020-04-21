e13b1fa29ece330f6fa9f6aa4da1756e181d3a8d
Mochamad Taufik Romdony <mtaufik.romdony@gmail.com>
2020-04-21 18:32:21 +0700

    Update pretext slack

--- Imsak.sh
+++ Imsak.sh
@@ -1,1 +1,1 @@
-curl -X POST https://hooks.slack.com/services/xxxx/xxxxx -d '{"username": "Haji Toped", "text": "10 menit menuju Sholat *Imsak : 04:27* :mosque:", "icon_emoji": ":kaaba:"}'
+curl -X POST https://hooks.slack.com/services/xxxx/xxxxx -d '{"username": "Haji Toped", "color": "#36a64f", "pretext": "*10 menit* menuju waktu *Imsak* :mosque:", "text": "\n\n *Imsak : 04:27*", "icon_emoji": ":kaaba:"}'
