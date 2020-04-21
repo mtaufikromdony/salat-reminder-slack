c8ea8f240c7cdf0cdf9c374a5ac0e3a07365ec18
Mochamad Taufik Romdony <mtaufik.romdony@gmail.com>
2020-04-21 18:31:51 +0700

    Update pretext slack

--- Asar.sh
+++ Asar.sh
@@ -1,1 +1,1 @@
-curl -X POST https://hooks.slack.com/services/xxxx/xxxxx -d '{"username": "Haji Toped", "text": "10 menit menuju Sholat *Asar : 15:14* :mosque:", "icon_emoji": ":kaaba:"}'
+curl -X POST https://hooks.slack.com/services/xxxx/xxxxx -d '{"username": "Haji Toped", "color": "#36a64f", "pretext": "*10 menit* menuju waktu Salat *Asar* :mosque:", "text": "\n\n *Asar : 15:14*", "icon_emoji": ":kaaba:"}'
